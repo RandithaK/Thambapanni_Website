aebf419d0914e746095f665893c810e60690be44
RandithaK <bharkula3@gmail.com>
2025-02-22 23:23:04 +0530

    Code Cleanup

--- Thambapanni_Web/src/App.tsx
+++ Thambapanni_Web/src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      
+
       <body>
         <h1 className="text-3xl font-bold">Thambapanni App</h1>
       </body>
